a1393b2ff540d9e9f956f26c4a8dd183d30a4354
Gworks GitLab CI <gworks-ci@gitlab-ci.com>
2022-11-30 14:46:31 +0000

    Latest version 'master_4f635722' deployment

--- variables.tf
+++ variables.tf
@@ -66,5 +66,5 @@ variable "ecs_php_app" {
 }
 
 variable "php_app_image" {
-  default = "141204083709.dkr.ecr.eu-north-1.amazonaws.com/php_app-prod:master_ab290d7b"
+  default = "141204083709.dkr.ecr.eu-north-1.amazonaws.com/php_app-prod:master_4f635722"
 }
